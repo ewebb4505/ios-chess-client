38178d3558fae5ce0327846f106b4f4b0cab340b
ewebb4505 <ewebb4505@gmail.com>
2024-05-04 12:37:17 -0400

    initial project creation

---
+++ ChessClient/ChessClient/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChessClient
+//
+//  Created by Evan Webb on 5/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
