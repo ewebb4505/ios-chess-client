7190e9602260f9788f8ab9fca4d160c4d0da1b30
ewebb4505 <ewebb4505@gmail.com>
2024-05-05 12:36:10 -0400

    setting up more chess game types and helper funcs

--- ChessClient/ChessClient/Core/Chess/Board.swift
+++ ChessClient/ChessClient/Core/Chess/Board.swift
@@ -11,17 +11,40 @@ typealias Board = [Spot : Piece?]
 typealias Pieces = [Piece : [Spot]?]
 
 extension Board {
-    func setBoard() -> Self {
-        //TODO: set rest of board
+    static func setBoard() -> Self {
         var board: Self = [:]
-        board[.a8] = .R; board[.b8] = .R; board[.c8] = .R; board[.d8] = .R; board[.e8] = .R; board[.f8] = .R; board[.g8] = .R; board[.h8] = .R
+        board[.a8] = .R; board[.b8] = .N; board[.c8] = .B; board[.d8] = .Q; board[.e8] = .K; board[.f8] = .B; board[.g8] = .N; board[.h8] = .R
         board[.a7] = .P; board[.b7] = .P; board[.c7] = .P; board[.d7] = .P; board[.e7] = .P; board[.f7] = .P; board[.g7] = .P; board[.h7] = .P
         board[.a6] = nil; board[.b6] = nil; board[.c6] = nil; board[.d6] = nil; board[.e6] = nil; board[.f6] = nil; board[.g6] = nil; board[.h6] = nil
         board[.a5] = nil; board[.b5] = nil; board[.c5] = nil; board[.d5] = nil; board[.e5] = nil; board[.f5] = nil; board[.g5] = nil; board[.h5] = nil
         board[.a4] = nil; board[.b4] = nil; board[.c4] = nil; board[.d4] = nil; board[.e4] = nil; board[.f4] = nil; board[.g4] = nil; board[.h4] = nil
         board[.a3] = nil; board[.b3] = nil; board[.c3] = nil; board[.d3] = nil; board[.e3] = nil; board[.f3] = nil; board[.g3] = nil; board[.h3] = nil
         board[.a2] = .P; board[.b2] = .P; board[.c2] = .P; board[.d2] = .P; board[.e2] = .P; board[.f2] = .P; board[.g2] = .P; board[.h2] = .P
-        board[.a1] = .R; board[.b1] = .R; board[.c1] = .R; board[.d1] = .R; board[.e1] = .R; board[.f1] = .R; board[.g1] = .R; board[.h1] = .R
+        board[.a1] = .R; board[.b1] = .N; board[.c1] = .B; board[.d1] = .Q; board[.e1] = .K; board[.f1] = .B; board[.g1] = .N; board[.h1] = .R
         return board
+    }
+}
+
+extension Pieces {
+    static func setPiecesForWhite() -> Self {
+        var pieces: Self = [:]
+        pieces[Piece.P] = [.a2,.b2,.c2,.d2,.e2,.f2,.g2,.h2]
+        pieces[Piece.R] = [.a1,.h1]
+        pieces[Piece.N] = [.b1,.g1]
+        pieces[Piece.B] = [.c1,.f1]
+        pieces[Piece.K] = [.e1]
+        pieces[Piece.Q] = [.d1]
+        return pieces
+    }
+    
+    static func setPiecesForBlack() -> Self {
+        var pieces: Self = [:]
+        pieces[Piece.P] = [.a7,.b7,.c7,.d7,.e7,.f7,.g7,.h7]
+        pieces[Piece.R] = [.a8,.h8]
+        pieces[Piece.N] = [.b8,.g8]
+        pieces[Piece.B] = [.c8,.f8]
+        pieces[Piece.K] = [.e8]
+        pieces[Piece.Q] = [.d8]
+        return pieces
     }
 }
--- ChessClient/ChessClient/Core/Chess/Spot.swift
+++ ChessClient/ChessClient/Core/Chess/Spot.swift
@@ -17,3 +17,46 @@ enum Spot: String, CaseIterable {
          a2, b2, c2, d2, e2, f2, g2, h2,
          a1, b1, c1, d1, e1, f1, g1, h1
 }
+
+extension Spot {
+    static func construct(row: Int, file: String) -> Spot? {
+        Spot(rawValue: row.description + file)
+    }
+    
+    var row: Int {
+        Int(self.rawValue[self.rawValue.endIndex].description)!
+    }
+    
+    var file: String {
+        self.rawValue[self.rawValue.startIndex].lowercased()
+    }
+    
+    var files: [String] {
+        ["a", "b", "c", "d", "e", "f", "g", "h"]
+    }
+    
+    func move(up: Int = 0, down: Int = 0, left: Int = 0, right: Int = 0) -> Self? {
+        guard (up > 8 || down > 8 || left > 8 || right > 8) else {
+            return nil
+        }
+        
+        let num = self.row
+        let file = self.file
+        guard let currentFileIndex = self.files.firstIndex(of: file)?.advanced(by: 0) else {
+            return nil
+        }
+        
+        let isStillInBounds = (num + up <= 8)
+        && (num - down >= 1)
+        && ((currentFileIndex+1) - left >= 1)
+        &&  ((currentFileIndex+1) + right <= 8)
+        
+        guard isStillInBounds else { return nil }
+        
+        let newNum = num + (up - down)
+        let newFileIndex = currentFileIndex + (right - left)
+        let newFile = self.files[newFileIndex]
+        
+        return Self.construct(row: newNum, file: newFile)
+    }
+}
